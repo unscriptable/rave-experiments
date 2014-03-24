0697390e43a29cea923d88cab865b2f6ee78fcb6
unscriptable <john@unscriptable.com>
2014-03-24 16:10:00 -0400

    Add an async require() to amd experiment.

--- amd/write.js
+++ amd/write.js
@@ -1,10 +1,7 @@
 define(function (require) {
 
-var domReady = require('curl/src/curl/domReady');
-var callbacks = require('when/callbacks');
-
 return function (msg, tagType) {
-	callbacks.call(domReady).then(function () {
+	require(['curl/src/curl/domReady'], function () {
 		var doc = document;
 		var body = doc.body;
 		body.appendChild(doc.createElement(tagType || 'p')).innerHTML = msg;
