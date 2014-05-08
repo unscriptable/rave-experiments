1fbc4387af0034f2aaa31cbc402a6ad1fec5b863
unscriptable <john@unscriptable.com>
2014-05-08 16:47:45 -0400

    Add angular experiment to test rave-angular extension.

---
+++ angular/app/app.js
@@ -0,0 +1,28 @@
+require('./filters');
+require('./services');
+require('./directives');
+require('./controllers');
+
+angular
+	.module('myApp', [
+		'ngRoute',
+		'myApp.filters',
+		'myApp.services',
+		'myApp.directives',
+		'myApp.controllers'
+	])
+	.config([
+		'$routeProvider', function ($routeProvider) {
+			$routeProvider.when(
+				'/view1',
+				{ templateUrl: 'app/view1/template.html', controller: 'MyCtrl1' }
+			);
+			$routeProvider.when(
+				'/view2',
+				{ templateUrl: 'app/view2/template.html', controller: 'MyCtrl2' }
+			);
+			$routeProvider.otherwise({redirectTo: '/view1'});
+		}
+	]);
+
+angular.bootstrap(document, ['myApp']);
---
+++ angular/app/controllers.js
@@ -0,0 +1,11 @@
+angular.module('myApp.controllers', [])
+	.controller('MyCtrl1', [
+		'$scope', function ($scope) {
+
+		}
+	])
+	.controller('MyCtrl2', [
+		'$scope', function ($scope) {
+
+		}
+	]);
