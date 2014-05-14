7a3f3195313d9096cf4517e5893044235361d45c
unscriptable <john@unscriptable.com>
2014-05-13 21:31:28 -0400

    Add test to ensure that css overrides are applied correctly and before imported by a module.

--- extension/displayMetadata.js
+++ extension/displayMetadata.js
@@ -6,13 +6,19 @@ var cssOverrides = require('./override-theme.css');
 
 // style sheet is editable here!
 cssOverrides.insertRule
-	? cssOverrides.insertRule('p { text-decoration: underline; }', cssOverrides.rules.length)
+	? cssOverrides.insertRule('p { text-decoration: underline; }', cssOverrides.cssRules.length)
 	: cssOverrides.addRule('p', 'text-decoration: underline;');
+
+var testOverrides = (function (p) {
+	p.className = 'test-override';
+	return p.offsetWidth === 7;
+}(document.body.appendChild(document.createElement('p'))));
 
 function displayMetadata (context, write) {
 	writeAppInfo(context.app, write);
 	write('Rave configured the following packages:');
 	write(formatPackages(context.packages), 'pre');
+	write('CSS overrides applied correctly? ' + testOverrides);
 	console.log(context);
 }
 
